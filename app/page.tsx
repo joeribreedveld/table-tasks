eabc9e0255a2fc1eee05b25e049624419dbdafa4
joeribreedveld <joeri.breedveld123@gmail.com>
2024-08-23 21:28:22 +0200

    feat: header with user-nav on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import { UserNav } from "@/components/user-nav";
+
 export default function Page() {
-  return <main>Hello, World!</main>;
+  return (
+    <main className="py-16 container">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+          <p className="text-muted-foreground">
+            Here&apos;s a list of your tasks for this month!
+          </p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <UserNav />
+        </div>
+      </div>
+    </main>
+  );
 }
