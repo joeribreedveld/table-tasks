a1df8a95ddb251e1955fe060d4db7259053b76bf
joeribreedveld <joeri.breedveld123@gmail.com>
2024-08-23 22:03:01 +0200

    feat: add shadcn-ui tasks frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,43 @@
+import { taskSchema } from "@/app/data/schema";
+import { columns } from "@/components/columns";
+import { DataTable } from "@/components/data-table";
 import { UserNav } from "@/components/user-nav";
+import { promises as fs } from "fs";
+import path from "path";
+import { z } from "zod";
 
-export default function Page() {
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "app/data/tasks.json"),
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function Page() {
+  const tasks = await getTasks();
+
   return (
-    <main className="py-16 container">
-      <div className="flex items-center justify-between space-y-2">
+    <main className="sm:py-16 py-12 sm:container space-y-8 px-4">
+      <div className="flex items-center justify-between space-y-2 gap-8">
         <div>
-          <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-          <p className="text-muted-foreground">
+          <h2 className="sm:text-2xl text-xl font-bold tracking-tight">
+            Welcome back!
+          </h2>
+
+          <p className="text-muted-foreground text-sm sm:text-base">
             Here&apos;s a list of your tasks for this month!
           </p>
         </div>
+
         <div className="flex items-center space-x-2">
           <UserNav />
         </div>
       </div>
+
+      <DataTable data={tasks} columns={columns} />
     </main>
   );
 }
--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -21,36 +21,34 @@ export function UserNav() {
               src="https://github.com/joeribreedveld.png"
               alt="@joeribreedveld"
             />
+
             <AvatarFallback>JB</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">Joeri Breedveld</p>
+
             <p className="text-xs leading-none text-muted-foreground">
               joeri.breedveld123@gmail.com
             </p>
           </div>
         </DropdownMenuLabel>
+
         <DropdownMenuSeparator />
+
         <DropdownMenuGroup>
           <DropdownMenuItem>
             Profile
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
+
         <DropdownMenuSeparator />
+
         <DropdownMenuItem>
           Log out
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
